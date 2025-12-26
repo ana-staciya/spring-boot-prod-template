e2579c1e8a104d2b9f9e92f95d73abd0fdccc190
Keiko <anastaciya.migal@gmail.com>
2025-12-26 11:48:28 -0500

    Changed the Spring Boot tests config and rename the path

--- src/main/java/io/github/anastaciya/sb_template/Application.java
+++ src/main/java/io/github/anastaciya/sb_template/Application.java
@@ -1,4 +1,4 @@
-package com.example.template;
+package io.github.anastaciya.sb_template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
